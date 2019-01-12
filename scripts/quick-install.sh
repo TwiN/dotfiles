ead0b0c6147f89bd2de499d5988d3dd1239657e2
TwiN <twin@twinnation.org>
2019-01-12 15:34:13 +0000

    Add wpa_supplicant and iw as packages to install

--- scripts/quick-install.sh
+++ scripts/quick-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo -e "\e[32mInstalling some necessary packages\e[0m"
-sudo pacman -Sy vim base-devel go firefox-developer-edition mlocate
+sudo pacman -Sy vim base-devel go firefox-developer-edition mlocate wpa_supplicant iw
 
 echo -e "\e[32mUpdating mlocate's database (use locate to search for a file)\e[0m"
 sudo updatedb
-
 
 echo -e "\e[32mInstalling yay as AUR helper\e[0m"
 git clone https://aur.archlinux.org/yay.git
