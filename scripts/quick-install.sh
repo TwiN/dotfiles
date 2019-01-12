771cb21b9317a4daef9913d66750a6d19de89457
TwiN <twin@twinnation.org>
2019-01-12 15:25:04 +0000

    Create quick-install.sh

---
+++ scripts/quick-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -e "\e[32mInstalling some necessary packages\e[0m"
+sudo pacman -Sy vim base-devel go firefox-developer-edition
+
+echo -e "\e[32mInstalling yay as AUR helper\e[0m"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+echo "Cleaning up"
+rm -rf yay
