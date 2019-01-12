d6ea11004778c8f70f8ea7baf4ad7dab2329ea8a
TwiN <twin@twinnation.org>
2019-01-12 15:30:49 +0000

    Update mlocate's file database

--- scripts/quick-install.sh
+++ scripts/quick-install.sh
@@ -2,6 +2,10 @@
 echo -e "\e[32mInstalling some necessary packages\e[0m"
 sudo pacman -Sy vim base-devel go firefox-developer-edition mlocate
 
+echo -e "\e[32mUpdating mlocate's database (use locate to search for a file)\e[0m"
+sudo updatedb
+
+
 echo -e "\e[32mInstalling yay as AUR helper\e[0m"
 git clone https://aur.archlinux.org/yay.git
 cd yay
