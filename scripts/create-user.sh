1853fb62f1ce337af02663ba72c82d09660f41ea
TwiN <twin@twinnation.org>
2019-01-12 17:33:10 -0500

    Minor visual enhancement

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+echo -e "\e[32m> Creating user 'twin' in group 'wheel'\e[0m"
 useradd -m -G wheel -s /bin/bash twin
 
 if ! [ -x "$(command -v sudo)" ]; then
-    echo -e "\e[32mInstalling sudo\e[0m"
+    echo -e "\e[32m> Installing sudo\e[0m"
     pacman -Sy sudo
 fi
 
-echo -e "\e[32mAdding wheel group to sudoers\e[0m"
+echo -e "\e[32m> Adding wheel group to sudoers\e[0m"
 echo "%wheel ALL=(ALL) ALL" | sudo tee -a /etc/sudoers >> /dev/null
