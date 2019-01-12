13f312ecec1b8f12f17e92ab26843e17b29b365a
TwiN <twin@twinnation.org>
2019-01-12 17:57:06 +0000

    Update quick-install.sh

--- scripts/quick-install.sh
+++ scripts/quick-install.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
-echo -e "\e[32mInstalling some necessary packages\e[0m"
-sudo pacman -Sy vim base-devel go firefox-developer-edition mlocate wpa_supplicant iw
 
-echo -e "\e[32mUpdating mlocate's database (use locate to search for a file)\e[0m"
-sudo updatedb
+confirm() {
+    read -r -p "${1:-Are you sure?} [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
 
-echo -e "\e[32mInstalling yay as AUR helper\e[0m"
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-echo "Cleaning up"
-rm -rf yay
+if confirm "Install base packages (vim base-devel go mlocate wpa_supplicant iw git) (recommended)?"; then
+    echo -e "\e[32mInstalling some necessary packages\e[0m"
+    sudo pacman -Sy vim base-devel go mlocate wpa_supplicant iw git
+    
+    echo -e "\e[32mSetting GOPATH and GOBIN to /go and /go/bin respectively in /etc/environment\e[0m"
+    echo 'GOPATH="/go"' | sudo tee -a /etc/environment >> /dev/null
+    echo 'GOBIN="/go/bin"' | sudo tee -a /etc/environment >> /dev/null
+
+    echo -e "\e[32mUpdating mlocate's database (use locate to search for a file)\e[0m"
+    sudo updatedb
+fi
+
+confirm "Install openjdk?" && sudo pacman -Sy jdk-openjdk
+confirm "Install firefox-developer-edition?" && sudo pacman -Sy firefox-developer-edition
+confirm "Install aws-cli?" && sudo pacman -Sy aws-cli
+
+if confirm "Install yay as AUR helper?"; then
+    echo -e "\e[32mInstalling yay as AUR helper\e[0m"
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    echo "Cleaning up"
+    rm -rf yay
+fi
+
+confirm "Install xorg and xorg-xinit?" && sudo pacman -Sy xorg xorg-xinit
+
