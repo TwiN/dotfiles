4a9935159299a1fd73a4cea0dcdb3075d30a5a59
TwinProduction <twin@twinnation.org>
2019-01-15 23:17:09 -0500

    Add polybar and desktop wallpaper

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar1 &
-polybar bar2 &
+polybar example &
+polybar 2 &
 
-echo "Started bar 1 and 2"
