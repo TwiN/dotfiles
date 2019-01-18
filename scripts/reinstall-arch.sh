af8e8aa6b5c585bcd8ba64b578831462d7dfc83f
TwiN <twin@twinnation.org>
2019-01-18 15:19:30 -0500

    Fix issue with script not running after chroot

--- scripts/reinstall-arch.sh
+++ scripts/reinstall-arch.sh
@@ -12,7 +12,16 @@ rm -rf /mnt/*
 pacstrap /mnt
 mount /dev/nvme0n1p1 /mnt/boot
 
-arch-chroot /mnt
-pacman -Sy vim iw wget git dialog wpa_supplicant
+# Create a script with the necessary programs to finish the installation
+# This is necessary because 'chroot' opens up a new interactive shell in
+# the new root, meaning that nothing after the 'arch-chroot /mnt' command
+# would run in that new environment; it would run in the old environment
+# instead
+cat << EOF > /mnt/FINISH_INSTALL.sh
+#!/bin/bash
+pacman -Sy --needed vim iw wget git dialog wpa_supplicant
+echo -e "\e[92mIf there's anything else you want to install, install it now. When you're done, type exit and reboot\e[0m"
+EOF
 
-echo "If there's anything else you want to install, install it now. When you're done, type exit and reboot"
+echo -e "\e[92mPlease run 'chmod +x FINISH_INSTALL.sh' followed by './FINISH_INSTALL.sh'\e[0m"
+arch-chroot /mnt
