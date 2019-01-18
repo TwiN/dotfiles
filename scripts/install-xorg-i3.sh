d69768246c4418ced01fd8be52b04428564acfbc
TwiN <twin@twinnation.org>
2019-01-18 17:33:53 -0500

    Add polybar install

--- scripts/install-xorg-i3.sh
+++ scripts/install-xorg-i3.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo -e "\e[32m> Installing xorg and xorg-xinit if they aren't already installed\e[0m"
+if ![ -x "$(command -v yay)" ]; then
+  echo 'You must install yay to run this script' >&2
+  exit 1
+fi
+
+echo -e "\e[32m> Installing xorg and xorg-xinit\e[0m"
 sudo pacman -Sy --needed xorg xorg-xinit
 
 echo -e "\e[32m> Installing i3\e[0m"
 sudo pacman -Sy --needed i3
 
-echo -e "\e[32m> Installing dmenu and ranger\e[0m"
+echo -e "\e[32m> Installing dmenu\e[0m"
 sudo pacman -Sy --needed dmenu
+
+echo -e "\e[32m> Installing polybar\e[0m"
+sudo pacman -Sy --needed polybar
