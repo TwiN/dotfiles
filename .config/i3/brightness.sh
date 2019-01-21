632c78f57643ceaacba8f8a422f602e79854c770
TwinProduction <twin@twinnation.org>
2019-01-20 23:45:51 -0500

    Fix brightness not updating on polybar

---
+++ .config/i3/brightness.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+############################################################################# 
+# This script "resolves" a bug with polybar's backlight module not updating #
+# when the brightness changes.                                              #
+#                                                                           #
+# See https://github.com/jaagr/polybar/issues/1180                          #
+#############################################################################
+
+sleep 1;
+cat /sys/class/backlight/intel_backlight/brightness
+
+
