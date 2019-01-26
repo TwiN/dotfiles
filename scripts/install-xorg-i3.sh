644986ad6f10ef447c4a5ea6a43653644e9ed67f
TwinProduction <twin@twinnation.org>
2019-01-26 16:24:23 -0500

    Replace dmenu for rofi

--- scripts/install-xorg-i3.sh
+++ scripts/install-xorg-i3.sh
@@ -8,14 +8,14 @@ fi
 echo -e "\e[32m> Installing xorg and xorg-xinit\e[0m"
 sudo pacman -Sy --needed xorg xorg-xinit
 
-echo -e "\e[32m> Installing i3\e[0m"
-sudo pacman -Sy --needed i3
+echo -e "\e[32m> Installing i3-gaps\e[0m"
+sudo pacman -S --needed i3-gaps
 
-echo -e "\e[32m> Installing dmenu\e[0m"
-sudo pacman -Sy --needed dmenu
+echo -e "\e[32m> Installing rofi\e[0m"
+sudo pacman -S --needed rofi
 
 echo -e "\e[32m> Installing polybar\e[0m"
 yay -S --needed polybar
 
 echo -e "\e[32m> Installing compton\e[0m"
-sudo pacman -Sy --needed compton
+sudo pacman -S --needed compton
