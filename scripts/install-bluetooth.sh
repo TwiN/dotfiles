b1aea9cf8a5263b1a16f7ecd22e763af69fe47a4
TwiN <twin@twinnation.org>
2019-06-11 21:46:21 -0400

    Use -Syu instead of -Sy

--- scripts/install-bluetooth.sh
+++ scripts/install-bluetooth.sh
@@ -2,11 +2,11 @@
 
 # This needs some work, I'm lazy right now so...
 
-sudo pacman -Sy bluez bluez-utils
+sudo pacman -Syu bluez bluez-utils
 
 # Should we really enable it out of the box? Waste of power
 #sudo systemctl enable bluetooth 
 
 # Bluetooth audio
-sudo pacman -S --needed pulseaudio-bluetooth
+sudo pacman -Syu --needed pulseaudio-bluetooth
 
