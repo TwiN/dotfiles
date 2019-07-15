b47dbe84033a2a8d57ab75183cb8b9dd99ee521a
TwinProduction <twin@twinnation.org>
2019-07-14 20:56:54 -0400

    Update instructions for connecting to a bluetooth device

--- scripts/install-bluetooth.sh
+++ scripts/install-bluetooth.sh
@@ -10,10 +10,19 @@ sudo pacman -Syu --needed bluez bluez-utils
 # Bluetooth audio
 sudo pacman -Syu --needed pulseaudio-bluetooth
 
+# Automatically start pulseaudio
+systemctl --user enable pulseaudio && systemctl --user start pulseaudio
+
 # Connecting to a bluetooth device:
-#systemctl restart bluetooth
+#sudo systemctl restart bluetooth
 #bluetoothctl
 #power on
 #scan on
+# If you've never paired with the device, then execute `pair <DEVICE_ID>`
+# before connecting to it.
 #connect <DEVICE_ID>
+
+# If that doesn't work, execute the following and try the steps above again:
+#sudo killall pulseaudio
+#pulseaudio --start
 
