7b6b995df0864bdc42de856c854b5b92f1b2baad
TwinProduction <twin@twinnation.org>
2019-10-14 19:55:17 -0400

    Start polybar for each monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar top &
-polybar bottom &
+# Start polybar on each monitors
+for monitor in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$monitor polybar top &
+	MONITOR=$monitor polybar bottom &
+done
+
